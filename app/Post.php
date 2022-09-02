501869fc9d217dbfa49765bf3ddc09e099f02e17
Leica Florian <florian.leica@gmail.com>
2022-09-02 11:43:50 +0200

    Adds crud views and controllers

--- app/Post.php
+++ app/Post.php
@@ -3,19 +3,24 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
+    use SoftDeletes;
+
     public function user()
     {
         return $this->belongsTo("App\User");
     }
 
-    public function category() {
+    public function category()
+    {
         return $this->belongsTo("App\Category");
     }
 
-    public function tags(){
+    public function tags()
+    {
         return $this->belongsToMany("App\Tag");
     }
 }
